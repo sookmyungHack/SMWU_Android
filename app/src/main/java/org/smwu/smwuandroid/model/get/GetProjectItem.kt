163497f91e16cc9ec5571ac60e74b8af0a46c8c1
sovovy <sovovy@gmail.com>
2018-11-03 04:48:58 +0900

    <메인 홈 새로운 프로젝트> 리사이클러뷰 아이템 완성

---
+++ app/src/main/java/org/smwu/smwuandroid/model/get/GetProjectItem.kt
@@ -0,0 +1,9 @@
+package org.smwu.smwuandroid.model.get
+
+data class GetProjectItem (
+        var title : String?,
+        var index1 : Int,
+        var img : String?,
+        var date : String?,
+        var category : Int
+)
